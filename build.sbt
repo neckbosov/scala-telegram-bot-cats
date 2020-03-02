eae5654f8a1cd2a56c45c687ef659ac9b6386e80
Nikita Bosov <neckbosov@gmail.com>
2020-03-02 17:38:46 +0300

    Add cat command

--- build.sbt
+++ build.sbt
@@ -5,4 +5,8 @@ version := "0.1"
 scalaVersion := "2.12.9"
 
 // Core with minimal dependencies, enough to spawn your first bot.
-libraryDependencies += "com.bot4s" %% "telegram-core" % "4.4.0-RC2"
+libraryDependencies ++= Seq(
+  "com.bot4s" %% "telegram-core" % "4.4.0-RC2",
+  "com.softwaremill.sttp" %% "json4s" % "1.7.2",
+  "org.json4s" %% "json4s-native" % "3.6.0"
+)
--- src/main/scala/BotStarter.scala
+++ src/main/scala/BotStarter.scala
@@ -1,3 +1,4 @@
+import Example.Response
 import cats.instances.future._
 import cats.syntax.functor._
 import com.bot4s.telegram.api.RequestHandler
@@ -5,16 +6,44 @@ import com.bot4s.telegram.api.declarative.Commands
 import com.bot4s.telegram.clients.{FutureSttpClient, ScalajHttpClient}
 import com.bot4s.telegram.future.{Polling, TelegramBot}
 import com.bot4s.telegram.models.User
-import com.softwaremill.sttp.SttpBackendOptions
+import com.softwaremill.sttp.{SttpBackend, SttpBackendOptions, sttp}
 import com.softwaremill.sttp.okhttp.OkHttpFutureBackend
+import org.json4s.native.Serialization
+import com.softwaremill.sttp.json4s._
 import slogging.{LogLevel, LoggerConfig, PrintLoggerFactory}
-
+import com.softwaremill.sttp._
 import scala.concurrent.duration.Duration
 import scala.util.Try
 import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.collection.mutable
+import scala.util.Random;
 
-class BotStarter(override val client: RequestHandler[Future]) extends TelegramBot
+case class Response(data: List[Data])
+
+case class Data(images: List[InnerData])
+
+case class InnerData(link: String)
+
+class Service(val apikey: String)(implicit val backend: SttpBackend[Future, Nothing], implicit val ec: ExecutionContext) {
+
+
+  private implicit val serialization: Serialization.type = org.json4s.native.Serialization
+
+  def getRandomCat(): Future[String] = {
+    val request = sttp
+      .header("Authorization", apikey)
+      .get(uri"https://api.imgur.com/3/gallery/search?q=cats")
+      .response(asJson[Response])
+
+    backend.send(request).map { response =>
+      val u = response.unsafeBody
+      val images = u.data.flatMap(_.images)
+      Random.shuffle(images).head.link
+    }
+  }
+}
+
+class BotStarter(override val client: RequestHandler[Future], val catsGetter: Service) extends TelegramBot
   with Polling
   with Commands[Future] {
   val users: mutable.Map[Int, User] = mutable.Map()
@@ -67,17 +96,24 @@ class BotStarter(override val client: RequestHandler[Future]) extends TelegramBo
       case None => Future.unit
     }
   }
+
+  onCommand("/cat") { implicit msg =>
+    catsGetter.getRandomCat().flatMap(link => reply(link)).void
+  }
 }
 
 object BotStarter {
   def main(args: Array[String]): Unit = {
     implicit val ec: ExecutionContext = ExecutionContext.global
-    implicit val backend = OkHttpFutureBackend(
+    implicit val backend: SttpBackend[Future, Nothing] = OkHttpFutureBackend(
       SttpBackendOptions.Default.socksProxy("ps8yglk.ddns.net", 11999)
     )
     val source = scala.io.Source.fromFile("apikey.txt")
-    val token = try source.mkString finally source.close()
-    val bot = new BotStarter(new FutureSttpClient(token))
+    val apiIter = source.getLines()
+    val token = apiIter.next()
+    val imgurApiKey = apiIter.next()
+    val bot = new BotStarter(new FutureSttpClient(token), new Service(imgurApiKey))
+    source.close()
     Await.result(bot.run(), Duration.Inf)
   }
 }
